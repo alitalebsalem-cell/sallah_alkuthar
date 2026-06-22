f94d461403719fcd48c780d77577160313f7dff5
alitalebsalem-cell <alitalebsalem@gmail.com>
2026-06-22 16:02:55 +0300

    Implement product saving functionality in admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,47 @@
-console.log("admin loaded");
+import { db } from "./firebase.js";
+
+import {
+collection,
+addDoc
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+document
+.getElementById("save")
+.addEventListener("click", async ()=>{
+
+const product = {
+
+name:
+document.getElementById("name").value,
+
+description:
+document.getElementById("description").value,
+
+code:
+document.getElementById("code").value,
+
+price:
+Number(
+document.getElementById("price").value
+),
+
+category:
+document.getElementById("category").value,
+
+image:
+document.getElementById("image").value,
+
+createdAt:
+Date.now()
+
+};
+
+await addDoc(
+collection(db,"products"),
+product
+);
+
+alert("تم إضافة المنتج");
+
+});
