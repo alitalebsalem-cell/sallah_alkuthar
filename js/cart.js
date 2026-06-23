b47007a475aaabbb0287f6eedcdc58c6b6097b8c
eng.ali <alitalebsalem@gmail.com>
2026-06-23 22:18:08 +0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -420,26 +420,53 @@ new window.jspdf.jsPDF(
 "mm",
 "A4"
 );
+const imgWidth = 210;
 
-const pageWidth = 210;
+const pageHeight = 297;
 
-const pageHeight =
-(canvas.height * pageWidth)
+const imgHeight =
+(canvas.height * imgWidth)
 / canvas.width;
+
+let heightLeft =
+imgHeight;
+
+let position = 0;
 
 pdf.addImage(
 imgData,
 "PNG",
 0,
-0,
-pageWidth,
-pageHeight
+position,
+imgWidth,
+imgHeight
 );
+
+heightLeft -= pageHeight;
+
+while(heightLeft > 0){
+
+position =
+heightLeft - imgHeight;
+
+pdf.addPage();
+
+pdf.addImage(
+imgData,
+"PNG",
+0,
+position,
+imgWidth,
+imgHeight
+);
+
+heightLeft -= pageHeight;
+
+}
 
 pdf.save(
 invoiceNo + ".pdf"
 );
-
 });
 
 if(cartSearch){
